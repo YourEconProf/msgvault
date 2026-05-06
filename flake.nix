992e4b2a6ef4d9f4e8ed63a6fcd8eb51bc8ba6e3
Wes McKinney <wesmckinn+git@gmail.com>
2026-05-05 21:05:48 -0500

    fix(nix): update vendorHash for go-pst dependency
    Adding github.com/mooijtech/go-pst/v6 to go.mod changed the vendor tree
    hash, breaking the nix-build CI job.

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
           pname = "msgvault";
           version = "0.14.1";
           src = ./.;
-          vendorHash = "sha256-EMQmrNZxb0EHZhgBDJBkQ3+X6ylZukfvG11M+kjhOWY=";
+          vendorHash = "sha256-rhijudB9WzFKtLAhfpuPw3osbo2lymTw1V+ZjuVyigU=";
           proxyVendor = true;
           subPackages = [ "cmd/msgvault" ];
           tags = [ "fts5" ];
